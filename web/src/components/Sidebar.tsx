f61a134438476a9ce70295650ac6f94953b50add
c ttps <67242476+cttps@users.noreply.github.com>
2024-08-15 21:56:18 -0700

    Fix two line sidebar for mobile

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
     return (
-        <div className="col-span-9 md:col-span-1 md:h-48 text-center grid grid-cols-5 md:grid-cols-1 place-items-center bg-white gap-0">
+        <div className="col-span-9 md:col-span-1 md:h-48 text-center grid grid-cols-6 md:grid-cols-1 place-items-center bg-white gap-0">
             <Link to="/">home</Link>
             <Link to="/about">about</Link>
             <Link to="/projects">projects</Link>
