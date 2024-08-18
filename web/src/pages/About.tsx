7254f9068d66dd196cc03f5c2f6f94ef012432ce
c ttps <67242476+cttps@users.noreply.github.com>
2024-08-18 12:33:55 -0700

    Update about me, favicon, title, accent color

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -8,6 +8,78 @@ export default function About() {
                 i've been coding since very young and tbh it has taken me a while to figure out if i really do love coding,
                 but building projects, solving problems, and learning seemingly impossible things is what i really enjoy and i think that's what  matters.
             </p>
+            <h1 className="text-xl font-semibold">programming life</h1>
+            <p>
+                i've basically grown up always with programming in some way or another i guess. a lot of memories
+                from my earliest friends are thinking about how to make games we thought of. i think i really first started
+                to learn how to code when i was 6, when i picked up python. not long after that i was told to use java
+                and i think that killed a lot of my desire to make the games that I wanted because it was too hard for me
+                to understand at my age (though not for some of my old friends lol). i didn't exactly code much until 6th grade
+                after that, i think the only other major milestone was learning to type a little faster and minecraft command blocks.
+            </p>
+            <p>
+                after that, it was mostly coding in batch/shell scripts, making unity games from tutorials, and making some dumb programs in visual basic along with
+                some scratch code from school. I think when i was bored and actually ended up making some stuff in php too for some reason.
+                at some point though, i discovered roblox (for a second time actually), and started
+                to learn about how to code games. it was really hard at first, and it took me a long, long time to understand
+                client-server communication. But once I did, i was really obsessed with coding. i also really liked working
+                with vfx sometime at the end of my roblox journey, but i think i've always wanted to just code what was in my head
+                at the time.
+            </p>
+            <p>
+                during this roblox phase, i tinkered with a lot of different programming languages. C, C# (via unity), C++, Java (which we
+                also learned in school, twice, and in uni again), javscript, and probably some more that i'm forgetting.
+                anyway, at some point in late high-school i discovered Go, and it was very similar to Lua (and js which i sort of liked at the time).
+                it was really easy to pick up, and i liked it a lot so I decided to try making stuff with it. honestly, i knew
+                nothing about the web at all, and just by randomly calling functions and searching on stack overflow, i made a website!
+                i think before that point, i did use express/MERN to make something, but i literally followed a tutorial for that and dont remember anything about it.
+            </p>
+            <p>
+                i did a lot with go tbh. I had a phase where i was learning game hacking (because at the time there was the whole thing
+                about riot vanguard and I wanted to know people were getting around the 'kernel level anticheat'). I used go to call C functions
+                i think, and made a few programs to modify the assembly code in some old games I had on steam. i learned a bit of x86 through that actually,
+                and it's been probably one of the more useful things i guess? i already understood pointers but coding some things in assembly gave me an understanding
+                of registers that was different from like the basic MAR/MDR that we had learned in class. this phase died out tho unfortunately, but i had another
+                hackery phase about privacy where i made my own encryption system for them in go. i've stuck with go since, and i love it a lot.
+            </p>
+            <p>
+                in uni, a lot of the content (esp in the first year) was stuff i already knew. I didn't really try hard at all and got decent grades throughout it i guess.
+                uni, until recently, had been a journey for my personal development mostly. i barely coded, and with chatgpt rolling out after covid, there
+                wasn't really anything i had to do to stay ontop of course work. at this point, its been maybe 4 years since i've been journaling, and its purpose has always been
+                to help me figure out what I want to do with my life. i read up on random articles about this stuff a lot and never had a satisfying answer. but i think
+                that i probably wont ever find out, and i've grown ok with that. i've started to code more often now, and it's been really fun. i really had to push myself to learn
+                things i didnt understand, but when i did, i realized i could actually do it. this has been a mentality that i've kept and i know that i can learn hard things
+                now. I even try to go out of my way to learn things i dont understand, because it's what other people arent willing to do. it's been fun!
+            </p>
+            <p>
+                now, well i guess for a while, whats bugging me the most is AI. not really the advent of agi/asi, but where it stands now and in the near future.
+                i finally rekindled my love for code that i lost 2 years ago, but current llms can basically code whatever project i can think up. i've made projects
+                like that, and it isn't as fun as me coding myself. i'm not learning much, and it's faster not to. If i have an error, i can just dump the logs and code
+                into a free llm and add their suggested fix. this is the point it's at now. i have genuine fear that software development, wont be coding anymore.
+                the coding part is what's fun to me. unless im at the very front of the pack, building something that ai isn't taught, i dont think i'll be able to even
+                find work without having to code using ai, and if that happens, i wont be able to enjoy this thing that i've been passionate about my whole life.
+                well, if we get asi sooner, or agi thats able to learn new things, then my idea is that everyone would be redundant. i want to make a post about this later,
+                but for now, the consequence is probably that everyone will just have to work a job that ai cant do, until ai can do it better.
+            </p>
+            <p>
+                this obviously means that very few people might actually enjoy their job.
+                so, my obvious conclusion is that i should work on getting asi here asap. theres also the fact current llms cant do novel research indepedently, so i'll even be able to
+                problem solve, write novel code, etc. it sounds like a great idea, but the people at the front of ai research are so smart that it seems impossible to be up there
+                with them. another solution is to work on something we are further behind on, like the robotics for machines that will embody ai.
+                i'm sure they're also very intelligent people too, but im assuming there's going to be work that needs to be done on the software side of things
+                that stays novel and unique. that assumption may not be true tho, and learning an entirely new part when ive dedicated myself to coding things much closer to the former
+                is something that's going to be pretty hard. for now, i'm focusing on learning ML and ai, with the idea of starting some side projects involving robotics soon.
+            </p>
+            <p>
+                so far, writing ml code has been done with llms. i'm taking the wrong approach because of that, but for now i just want to dip my toes into a field that i actually
+                was initially (like 3-4 years ago) against working in. I know im far behind right now, and it seems impossible to get up there with the researchers who are
+                probably some of the smartest on earth THIS late, but i don't really see any other choice for me so that's what im going to aim for, for now.
+            </p>
+            <p>
+                thanks for reading all this! if you did read through everything, please feel free to contact me. i'd love to talk about anything tech related, and maybe
+                i can get new insights.
+            </p>
+            <p>for future reference, this was written Aug 18, 2024.</p>
         </div>
     );
   }
