ca3fd85eea801a8a673340c0fa35a3feec948e17
c ttps <67242476+cttps@users.noreply.github.com>
2024-09-11 12:57:30 -0400

    Update projects

--- web/src/pages/Projects.tsx
+++ web/src/pages/Projects.tsx
@@ -1,4 +1,6 @@
 
+import { Link } from "react-router-dom";
+
 export default function Projects() {
     return (
         <>
@@ -6,17 +8,17 @@ export default function Projects() {
                 <h1 className='text-2xl my-2 mb-4'>projects</h1>
                 <div className="grid gap-2 w-full">
                     <div className="p-2 shadow-lg grid rounded-xl place-items-center border">
-                        <h2 className="text-xl my-2">this website</h2>
+                        <h2 className="text-xl my-2">cheatseat</h2>
                         <div className="grid text-justify w-full p-4 max-w-3xl overflow-scroll">
                             <p>
-                                This is my personal website. Not much special about it. It's using react tsx, tailwind, and go for the backend.
-                                Its pretty basic and ugly. The <a href="./omTimer">timer</a> page was pretty cool to make though.
-                                The hardest part was probably figuring out how to convert a percentage back into seconds relative to the start/current/end time.
+                                A website for university students to get email notifications for courses they select so that
+                                they can find a spot when its full. this was a pretty huge project to take on alone, but yeah it was like
+                                the most fun ive had in a while lol
                             </p>
                         </div>
                         <div className="grid place-items-center grid-cols-2 w-full text-center">
-                            <a className="px-3 py-1 bg-blue-50 rounded-xl shadow hover:px-[500px] hover:py-[300px] transition-all ease-out duration-[5s]" href="https://github.com/cttps/personalwebsite"> github </a>
-                            <a className="px-3 py-1 bg-blue-50 rounded-xl shadow hover:px-[500px] hover:py-[300px] transition-all ease-out duration-[5s]" href="./">link</a>
+                            <a className="px-3 py-1 bg-blue-50 rounded-xl shadow hover:px-[500px] hover:py-[300px] transition-all ease-out duration-[5s]">no github </a>
+                            <a className="px-3 py-1 bg-blue-50 rounded-xl shadow hover:px-[500px] hover:py-[300px] transition-all ease-out duration-[5s]" href="https://cheatseat.ca">link</a>
                         </div>
                     </div>
                     <div className="p-2 shadow-lg grid rounded-xl place-items-center border">
@@ -34,6 +36,20 @@ export default function Projects() {
                         <div className="grid place-items-center grid-cols-2 w-full text-center mt-2">
                             <a className="px-3 py-1 bg-blue-50 rounded-xl shadow hover:px-[500px] hover:py-[300px] transition-all ease-out duration-[5s]" href="https://github.com/mxpph/quickdraw-vs"> github </a>
                             <a className="px-3 py-1 bg-blue-50 rounded-xl shadow hover:px-[500px] hover:py-[300px] transition-all ease-out duration-[5s]">link</a>
+                        </div>
+                    </div>
+                    <div className="p-2 shadow-lg grid rounded-xl place-items-center border">
+                        <h2 className="text-xl my-2">this website</h2>
+                        <div className="grid text-justify w-full p-4 max-w-3xl overflow-scroll">
+                            <p>
+                                This is my personal website. Not much special about it. It's using react tsx, tailwind, and go for the backend.
+                                Its pretty basic and ugly. The <Link to="/omTimer" className="underline underline-offset-1">timer</Link> page was pretty cool to make though.
+                                The hardest part was probably figuring out how to convert a percentage back into seconds relative to the start/current/end time.
+                            </p>
+                        </div>
+                        <div className="grid place-items-center grid-cols-2 w-full text-center">
+                            <a className="px-3 py-1 bg-blue-50 rounded-xl shadow hover:px-[500px] hover:py-[300px] transition-all ease-out duration-[5s]" href="https://github.com/cttps/personalwebsite"> github </a>
+                            <a className="px-3 py-1 bg-blue-50 rounded-xl shadow hover:px-[500px] hover:py-[300px] transition-all ease-out duration-[5s]" href="./">recursion?</a>
                         </div>
                     </div>
                 </div>
